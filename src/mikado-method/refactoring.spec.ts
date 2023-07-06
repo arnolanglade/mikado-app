a37f7b9ba947d215a02c602983ef3e9d9956396d
arnaud <arnaud.langlade@gmail.com>
2023-07-06 19:41:08 +0200

    test: add a factory to build refactoring object

--- src/mikado-method/refactoring.spec.ts
+++ src/mikado-method/refactoring.spec.ts
@@ -1,26 +1,28 @@
 import {InMemoryRefactorings, Refactoring, startRefactoring, UnknownRefactoring} from '@/mikado-method/refactoring';
 
+const aRefactoring = (state: {id: string}) => new Refactoring(state.id)
+
 describe('Refactoring', () => {
   it('starts a refactoring', () => {
     const refactorings = new InMemoryRefactorings();
     startRefactoring(refactorings)({refactoringId: '51bb1ce3-d1cf-4d32-9d10-8eea626f4784'})
 
     expect(refactorings.get('51bb1ce3-d1cf-4d32-9d10-8eea626f4784'))
-        .toEqual(new Refactoring('51bb1ce3-d1cf-4d32-9d10-8eea626f4784'));
+        .toEqual(aRefactoring({id: '51bb1ce3-d1cf-4d32-9d10-8eea626f4784'}));
   });
 });
 
 describe('Refactoring', () => {
   describe('identifyBy', () => {
     it('says yes if the given id match the refactoring id', () => {
-      const refactoring = new Refactoring('51bb1ce3-d1cf-4d32-9d10-8eea626f4784');
+      const refactoring = aRefactoring({id: '51bb1ce3-d1cf-4d32-9d10-8eea626f4784'});
 
       expect(refactoring.identifyBy('51bb1ce3-d1cf-4d32-9d10-8eea626f4784'))
           .toEqual(true);
     });
 
     it('says no if the given id does not match the refactoring id', () => {
-      const refactoring = new Refactoring('51bb1ce3-d1cf-4d32-9d10-8eea626f4784');
+      const refactoring = aRefactoring({id: '51bb1ce3-d1cf-4d32-9d10-8eea626f4784'});
 
       expect(refactoring.identifyBy('c2e2ddf8-534b-4080-b47c-0f4536b54cae'))
           .toEqual(false);
@@ -32,14 +34,14 @@ describe('Refactorings', () => {
   it('persists a refactoring', () => {
     const refactorings = new InMemoryRefactorings();
 
-    refactorings.add(new Refactoring('51bb1ce3-d1cf-4d32-9d10-8eea626f4784'))
+    refactorings.add(aRefactoring({id: '51bb1ce3-d1cf-4d32-9d10-8eea626f4784'}))
 
     expect(refactorings.get('51bb1ce3-d1cf-4d32-9d10-8eea626f4784'))
-        .toEqual(new Refactoring('51bb1ce3-d1cf-4d32-9d10-8eea626f4784'));
+        .toEqual(aRefactoring({id: '51bb1ce3-d1cf-4d32-9d10-8eea626f4784'}));
   });
 
   it('raises an error if the given id does not match an existing refactoring', () => {
-    const refactorings = new InMemoryRefactorings([new Refactoring('51bb1ce3-d1cf-4d32-9d10-8eea626f4784')]);
+    const refactorings = new InMemoryRefactorings([aRefactoring({id: '51bb1ce3-d1cf-4d32-9d10-8eea626f4784'})]);
 
 
     expect(() => refactorings.get('c2e2ddf8-534b-4080-b47c-0f4536b54cae'))
