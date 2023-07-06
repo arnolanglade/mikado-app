e422fbdafe2e9d938bd567a57954717f141b8137
arnaud <arnaud.langlade@gmail.com>
2023-07-06 17:49:59 +0200

    refactor: add refactoring params to the add method

--- src/mikado-method/refactoring.ts
+++ src/mikado-method/refactoring.ts
@@ -7,17 +7,17 @@ export class Refactoring {
 
 export interface Refactorings {
     get(id: string): Refactoring
-    add(): void
+    add(refactoring: Refactoring): void
 }
 
 export class InMemoryRefactorings implements Refactorings {
     // eslint-disable-next-line class-methods-use-this
-    add(): void {
-    }
-
-    // eslint-disable-next-line class-methods-use-this
     get(id: string): Refactoring {
         return new Refactoring(id);
+    }
+
+    // eslint-disable-next-line class-methods-use-this,@typescript-eslint/no-unused-vars
+    add(refactoring: Refactoring): void {
     }
 }
 
