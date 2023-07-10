573627d2d5a339873c3b48fa0753370121644015
arnaud <arnaud.langlade@gmail.com>
2023-07-10 14:15:21 +0200

    feat: add the DI to the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { ServiceContainerProvider } from '@/lib/service-container-context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <ServiceContainerProvider>
+          {children}
+        </ServiceContainerProvider>
         <Analytics />
       </body>
     </html>
