648d37cf7970b9a85209217b0518806d62b66049
arnaud <arnaud.langlade@gmail.com>
2023-07-11 20:03:33 +0200

    fix: type issues

--- app/refactoring/use-case/refactoring.spec.tsx
+++ app/refactoring/use-case/refactoring.spec.tsx
@@ -4,16 +4,31 @@ import { act, renderHook } from '@testing-library/react';
 import useRefactoring from '@/refactoring/use-case/refactoring';
 import { jest } from '@jest/globals';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
-import { HttpClient } from '@/lib/http-client';
+import httpClient, { HttpClient } from '@/lib/http-client';
 import { createWrapper } from '@/lib/test-utils';
+
+const aHttpClient = (client: Partial<HttpClient>): HttpClient => ({
+  post: jest.fn() as jest.Mocked<typeof httpClient.post>,
+  get: jest.fn() as jest.Mocked<typeof httpClient.get>,
+  ...client,
+});
+const aRouter = (router: Partial<AppRouterInstance>): AppRouterInstance => ({
+  back: jest.fn(),
+  forward: jest.fn(),
+  refresh: jest.fn(),
+  push: jest.fn(),
+  replace: jest.fn(),
+  prefetch: jest.fn(),
+  ...router,
+});
 
 describe('useRefactoring', () => {
   test('The developer starts a refactoring', async () => {
-    const post = jest.fn();
+    const post = jest.fn() as jest.Mocked<typeof httpClient.post>;
     const push = jest.fn();
     const { result } = renderHook(useRefactoring, {
       wrapper: createWrapper({
-        httpClient: ({ post } as HttpClient), useRouter: () => ({ push } as AppRouterInstance),
+        httpClient: (aHttpClient({ post })), useRouter: () => (aRouter({ push })),
       }),
     });
 
