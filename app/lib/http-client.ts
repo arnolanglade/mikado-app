67b71816054e688be0d8071b47d9d9ff31238897
arnaud <arnaud.langlade@gmail.com>
2023-09-18 10:46:21 +0200

    fix(http-client): throw an exception when the response is not ok

--- app/lib/http-client.ts
+++ app/lib/http-client.ts
@@ -4,20 +4,36 @@ export type HttpClient = {
 };
 
 const httpClient: HttpClient = {
-  post: async <T>(route: string, data: T) => fetch(route, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  }),
-  get: async (route: string): Promise<Response> => fetch(route, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-    },
-  }),
+  post: async <T>(route: string, data: T) => {
+    const response = await fetch(route, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! ${response.status} (code: ${response.status}`);
+    }
+
+    return response;
+  },
+  get: async (route: string): Promise<Response> => {
+    const response = await fetch(route, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! ${response.status} (code: ${response.status}`);
+    }
+
+    return response;
+  },
 };
 export default httpClient;
