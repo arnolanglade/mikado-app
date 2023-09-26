9e4b2182aee870295fdfe6b2f72510b756fb08f7
arnaud <arnaud.langlade@gmail.com>
2023-09-26 18:24:58 +0200

    feat(refactoring): the page uses refactoring api

--- app/refactoring/[id]/page.tsx
+++ app/refactoring/[id]/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { getRefactoringById } from '@/api/refactoring/refactoring';
 import Page from '@/refactoring/component/refactoring-page';
+import refactoringApi from '@/refactoring/refactoring';
 import styles from './page.module.css';
 
 export default async function RefactoringPage({ params: { id } }: { params: { id: string } }) {
-  const refactoring = (await getRefactoringById(id));
+  const refactoring = (await refactoringApi.getById(id));
 
   return (
     <div className={styles.dashboard}>
