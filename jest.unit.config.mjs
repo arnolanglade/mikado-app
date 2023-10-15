6d963404061715aded45e020d02b79b2b927e38a
arnaud <arnaud.langlade@gmail.com>
2023-10-15 14:05:37 +0200

    refactor(jest): rework unit test config

--- jest.unit.config.mjs
+++ jest.unit.config.mjs
@@ -1,23 +1,19 @@
 import nextJest from 'next/jest.js'
 
-const createJestConfig = nextJest({
-    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-    dir: './',
-})
+const createJestConfig = nextJest({dir: './'})
 
-// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-    // Add more setup options before each test is run
-    // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/app/$1',
     },
-    testEnvironment: 'jest-environment-jsdom',
+    testEnvironment: 'jest-environment-jsdom', // Todo: should be node for api tests
     setupFilesAfterEnv: [
         '<rootDir>/test/jest.setup.ts',
-    ]
+    ],
+    testMatch: [
+        '**/*.spec.(ts|tsx)',
+    ],
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
