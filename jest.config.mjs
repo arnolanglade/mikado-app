877313e3105eb345990a8d58a8fd5e7108a76721
arnaud <arnaud.langlade@gmail.com>
2023-10-16 09:07:30 +0200

    test: keep only one jest config file
    It is easier to run test locally with intelliJ

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,13 +7,6 @@ const config = {
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/app/$1',
     },
-    testEnvironment: 'jest-environment-jsdom', // Todo: should be node for api tests
-    setupFilesAfterEnv: [
-        '<rootDir>/test/unit.setup.ts',
-    ],
-    testMatch: [
-        '**/*.spec.(ts|tsx)',
-    ],
 }
 
 export default createJestConfig(config)
